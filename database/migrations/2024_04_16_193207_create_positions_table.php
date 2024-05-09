ef53d16a42f90df8798ff47cade84314487635cd
dhsoft95 <65132687+dhsoft95@users.noreply.github.com>
2024-05-10 02:48:22 +0300

    ggg

--- database/migrations/2024_04_16_193207_create_positions_table.php
+++ database/migrations/2024_04_16_193207_create_positions_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('client_id');
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('other_position_details')->nullable();
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('oauth_personal_access_clients');
+        Schema::dropIfExists('positions');
     }
 };
