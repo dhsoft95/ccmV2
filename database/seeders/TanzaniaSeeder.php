9c275d746b4aef24c85d5d4992864c7a7dab3403
David John <65132687+dhsoft95@users.noreply.github.com>
2025-07-02 05:41:34 +0300

    Update

--- database/seeders/TanzaniaSeeder.php
+++ database/seeders/TanzaniaSeeder.php
@@ -61,98 +61,97 @@ class TanzaniaSeeder extends Seeder
     {
         $tanzaniaData = [
             'Arusha' => [
-                'Arusha City', 'Arusha Rural', 'Karatu', 'Longido', 'Monduli', 'Ngorongoro'
+                'Arusha City', 'Arusha Rural', 'Karatu', 'Longido', 'Meru', 'Monduli', 'Ngorongoro'
             ],
             'Dar es Salaam' => [
-                'Ilala', 'Kinondoni', 'Temeke', 'Ubungo', 'Kigamboni'
+                'Ilala Municipal', 'Kigamboni Municipal', 'Kinondoni Municipal', 'Temeke Municipal', 'Ubungo Municipal'
             ],
             'Dodoma' => [
-                'Dodoma Urban', 'Dodoma Rural', 'Bahi', 'Chamwino', 'Chemba', 'Kondoa', 'Kongwa', 'Mpwapwa'
+                'Bahi', 'Chamwino', 'Chemba', 'Dodoma City', 'Kondoa Rural', 'Kondoa Town', 'Kongwa', 'Mpwapwa'
             ],
             'Geita' => [
-                'Geita Town', 'Geita Rural', 'Bukombe', 'Chato', 'Mbogwe', 'Nyang\'hwale'
+                'Bukombe', 'Chato', 'Geita Rural', 'Geita Town', 'Mbogwe', 'Nyang\'hwale'
             ],
             'Iringa' => [
-                'Iringa Urban', 'Iringa Rural', 'Kilolo', 'Mafinga', 'Mufindi'
+                'Iringa Municipal', 'Iringa Rural', 'Kilolo', 'Mafinga Town', 'Mufindi'
             ],
             'Kagera' => [
-                'Bukoba Urban', 'Bukoba Rural', 'Biharamulo', 'Karagwe', 'Kyerwa', 'Misenyi', 'Muleba', 'Ngara'
-            ],
-            'Katavi' => [
-                'Mpanda Town', 'Mpanda Rural', 'Mlele'
-            ],
-            'Kigoma' => [
-                'Kigoma Urban', 'Kigoma Rural', 'Buhigwe', 'Kakonko', 'Kasulu', 'Kibondo', 'Uvinza'
-            ],
-            'Kilimanjaro' => [
-                'Moshi Urban', 'Moshi Rural', 'Hai', 'Rombo', 'Same', 'Siha'
-            ],
-            'Lindi' => [
-                'Lindi Urban', 'Lindi Rural', 'Kilifi', 'Liwale', 'Nachingwea', 'Ruangwa'
-            ],
-            'Manyara' => [
-                'Babati Town', 'Babati Rural', 'Hanang', 'Kiteto', 'Mbulu', 'Simanjiro'
-            ],
-            'Mara' => [
-                'Musoma Urban', 'Musoma Rural', 'Bunda', 'Butiama', 'Rorya', 'Serengeti', 'Tarime'
-            ],
-            'Mbeya' => [
-                'Mbeya City', 'Mbeya Rural', 'Busokelo', 'Chunya', 'Kyela', 'Mbarali', 'Rungwe'
-            ],
-            'Morogoro' => [
-                'Morogoro Urban', 'Morogoro Rural', 'Gairo', 'Kilombero', 'Kilosa', 'Malinyi', 'Mvomero', 'Ulanga'
-            ],
-            'Mtwara' => [
-                'Mtwara Urban', 'Mtwara Rural', 'Masasi', 'Nanyumbu', 'Newala', 'Tandahimba'
-            ],
-            'Mwanza' => [
-                'Ilemela', 'Nyamagana', 'Busega', 'Kwimba', 'Magu', 'Misungwi', 'Sengerema', 'Ukerewe'
-            ],
-            'Njombe' => [
-                'Njombe Town', 'Njombe Rural', 'Ludewa', 'Makambako', 'Makete', 'Wanging\'ombe'
-            ],
-            'Pwani' => [
-                'Kibaha Town', 'Kibaha Rural', 'Bagamoyo', 'Chalinze', 'Kisarawe', 'Mafia', 'Mkuranga', 'Rufiji'
-            ],
-            'Rukwa' => [
-                'Sumbawanga Urban', 'Sumbawanga Rural', 'Kalambo', 'Nkasi'
-            ],
-            'Ruvuma' => [
-                'Songea Urban', 'Songea Rural', 'Madaba', 'Mbinga', 'Namtumbo', 'Nyasa', 'Tunduru'
-            ],
-            'Shinyanga' => [
-                'Shinyanga Urban', 'Shinyanga Rural', 'Kahama Town', 'Kahama Rural', 'Kishapu', 'Msalala'
-            ],
-            'Simiyu' => [
-                'Bariadi Town', 'Bariadi Rural', 'Itilima', 'Maswa', 'Meatu'
-            ],
-            'Singida' => [
-                'Singida Urban', 'Singida Rural', 'Ikungi', 'Iramba', 'Manyoni', 'Mkalama'
-            ],
-            'Songwe' => [
-                'Mbozi', 'Momba', 'Songwe', 'Ileje'
-            ],
-            'Tabora' => [
-                'Tabora Urban', 'Tabora Rural', 'Igunga', 'Kaliua', 'Nzega', 'Sikonge', 'Urambo', 'Uyui'
-            ],
-            'Tanga' => [
-                'Tanga City', 'Tanga Rural', 'Handeni Town', 'Handeni Rural', 'Kilifi', 'Korogwe Town', 'Korogwe Rural', 'Lushoto', 'Mkinga', 'Muheza', 'Pangani'
-            ],
-            // Zanzibar regions
-            'Kaskazini Unguja' => [
-                'Kaskazini A', 'Kaskazini B'
-            ],
-            'Kusini Unguja' => [
-                'Kusini', 'Kusini Unguja'
-            ],
-            'Mjini Magharibi' => [
-                'Mjini', 'Magharibi A', 'Magharibi B'
+                'Biharamulo', 'Bukoba Municipal', 'Bukoba Rural', 'Karagwe', 'Kyerwa', 'Missenyi', 'Muleba', 'Ngara'
             ],
             'Kaskazini Pemba' => [
-                'Kaskazini Pemba', 'Micheweni'
+                'Micheweni', 'Wete'
+            ],
+            'Kaskazini Unguja' => [
+                'Kaskazini A Town', 'Kaskazini B'
+            ],
+            'Katavi' => [
+                'Mlele', 'Mpanda Municipal', 'Mpimbwe', 'Nsimbo', 'Tanganyika'
+            ],
+            'Kigoma' => [
+                'Buhigwe', 'Kakonko', 'Kasulu Rural', 'Kasulu Town', 'Kibondo', 'Kigoma Municipal', 'Kigoma Rural', 'Uvinza'
+            ],
+            'Kilimanjaro' => [
+                'Hai', 'Moshi Municipal', 'Moshi Rural', 'Mwanga', 'Rombo', 'Same', 'Siha'
             ],
             'Kusini Pemba' => [
-                'Kusini Pemba', 'Mkoani'
+                'Chake Chake', 'Mkoani Town'
+            ],
+            'Kusini Unguja' => [
+                'Kati', 'Kusini'
+            ],
+            'Lindi' => [
+                'Kilwa', 'Lindi Municipal', 'Liwale', 'Mtama', 'Nachingwea', 'Ruangwa'
+            ],
+            'Manyara' => [
+                'Babati Rural', 'Babati Town', 'Hanang', 'Kiteto', 'Mbulu Rural', 'Mbulu Town', 'Simanjiro'
+            ],
+            'Mara' => [
+                'Bunda Rural', 'Bunda Town', 'Butiama', 'Musoma Municipal', 'Musoma Rural', 'Rorya', 'Serengeti', 'Tarime Rural', 'Tarime Town'
+            ],
+            'Mbeya' => [
+                'Busekelo', 'Chunya', 'Kyela', 'Mbarali', 'Mbeya City', 'Mbeya Rural', 'Rungwe'
+            ],
+            'Mjini Magharibi' => [
+                'Magharibi A Municipal', 'Magharibi B Municipal', 'Mjini Municipal'
+            ],
+            'Morogoro' => [
+                'Gairo', 'Ifakara Town', 'Kilosa', 'Malinyi', 'Mlimba', 'Morogoro Municipal', 'Morogoro Rural', 'Mvomero', 'Ulanga'
+            ],
+            'Mtwara' => [
+                'Masasi Rural', 'Masasi Town', 'Mtwara Municipal', 'Mtwara Rural', 'Nanyamba Town', 'Nanyumbu', 'Newala Rural', 'Newala Town', 'Tandahimba'
+            ],
+            'Mwanza' => [
+                'Buchosa', 'Ilemela Municipal', 'Kwimba', 'Magu', 'Misungwi', 'Mwanza', 'Sengerema', 'Ukerewe'
+            ],
+            'Njombe' => [
+                'Ludewa', 'Makambako Town', 'Makete', 'Njombe Rural', 'Njombe Town', 'Wanging\'ombe'
+            ],
+            'Pwani' => [
+                'Bagamoyo', 'Chalinze', 'Kibaha', 'Kibaha Town', 'Kibiti', 'Kisarawe', 'Mafia', 'Mkuranga', 'Rufiji'
+            ],
+            'Rukwa' => [
+                'Kalambo', 'Nkasi', 'Sumbawanga Municipal', 'Sumbawanga Rural'
+            ],
+            'Ruvuma' => [
+                'Madaba', 'Mbinga Rural', 'Mbinga Town', 'Namtumbo', 'Nyasa', 'Songea Municipal', 'Songea Rural', 'Tunduru'
+            ],
+            'Shinyanga' => [
+                'Kahama Municipality', 'Kishapu', 'Msalala', 'Shinyanga Municipal', 'Shinyanga Rural', 'Ushetu'
+            ],
+            'Simiyu' => [
+                'Bariadi Rural', 'Bariadi Town', 'Busega', 'Itilima', 'Maswa', 'Meatu'
+            ],
+            'Singida' => [
+                'Ikungi', 'Iramba', 'Itigi', 'Manyoni', 'Mkalama', 'Singida Municipal', 'Singida Rural'
+            ],
+            'Songwe' => [
+                'Ileje', 'Mbozi', 'Momba', 'Songwe', 'Tunduma Town'
+            ],
+            'Tabora' => [
+                'Igunga', 'Kaliua', 'Nzega Rural', 'Nzega Town', 'Sikonge', 'Tabora Municipal', 'Urambo', 'Uyui'
+            ],
+            'Tanga' => [
+                'Bumbuli', 'Handeni Rural', 'Handeni Town', 'Kilindi', 'Korogwe Rural', 'Korogwe Town', 'Lushoto', 'Mkinga', 'Muheza', 'Pangani', 'Tanga City'
             ]
         ];
 
@@ -186,24 +185,31 @@ class TanzaniaSeeder extends Seeder
     {
         echo "Seeding sample villages...\n";
 
-        // Districts where we'll create wards (major urban centers)
+        // Major districts where we'll create wards (expanded list)
         $districtsWithWards = [
-            'Ilala', 'Kinondoni', 'Temeke', 'Ubungo', 'Kigamboni',  // Dar es Salaam
-            'Arusha City',  // Arusha
-            'Ilemela', 'Nyamagana',  // Mwanza
-            'Dodoma Urban',  // Dodoma
-            'Mbeya City',  // Mbeya
-            'Moshi Urban',  // Kilimanjaro
-            'Iringa Urban',  // Iringa
-            'Bukoba Urban',  // Kagera
-            'Kigoma Urban',  // Kigoma
-            'Tanga City',  // Tanga
-            'Morogoro Urban',  // Morogoro
-            'Mtwara Urban',  // Mtwara
-            'Shinyanga Urban',  // Shinyanga
-            'Singida Urban',  // Singida
-            'Tabora Urban',  // Tabora
-            'Mjini'  // Zanzibar
+            // Dar es Salaam
+            'Ilala Municipal', 'Kinondoni Municipal', 'Temeke Municipal', 'Ubungo Municipal', 'Kigamboni Municipal',
+
+            // Major cities
+            'Arusha City', 'Dodoma City', 'Mbeya City', 'Tanga City',
+
+            // Major urban centers
+            'Moshi Municipal', 'Iringa Municipal', 'Bukoba Municipal', 'Kigoma Municipal',
+            'Morogoro Municipal', 'Mtwara Municipal', 'Shinyanga Municipal', 'Singida Municipal',
+            'Tabora Municipal', 'Sumbawanga Municipal', 'Songea Municipal',
+
+            // Mwanza districts
+            'Ilemela Municipal', 'Mwanza',
+
+            // Zanzibar
+            'Mjini Municipal', 'Magharibi A Municipal', 'Magharibi B Municipal',
+
+            // Other major towns
+            'Musoma Municipal', 'Kasulu Town', 'Babati Town', 'Makambako Town',
+            'Njombe Town', 'Kibaha Town', 'Geita Town', 'Mafinga Town',
+
+            // Special case - Rungwe (since you had its ward data)
+            'Rungwe'
         ];
 
         foreach ($districtsWithWards as $districtName) {
@@ -233,19 +239,19 @@ class TanzaniaSeeder extends Seeder
 
         $wardData = [
             // Dar es Salaam wards
-            'Ilala' => [
+            'Ilala Municipal' => [
                 'Buguruni', 'Gerezani', 'Ilala', 'Jangwani', 'Kariakoo', 'Kisutu', 'Mchikichini', 'Upanga East', 'Upanga West'
             ],
-            'Kinondoni' => [
+            'Kinondoni Municipal' => [
                 'Hananasif', 'Kawe', 'Kinondoni', 'Mabibo', 'Magomeni', 'Makongo', 'Manzese', 'Msasani', 'Sinza'
             ],
-            'Temeke' => [
+            'Temeke Municipal' => [
                 'Chang\'ombe', 'Keko', 'Kurasini', 'Mbagala', 'Miburani', 'Mtoni', 'Sandali', 'Temeke', 'Vijibweni'
             ],
-            'Ubungo' => [
+            'Ubungo Municipal' => [
                 'Goba', 'Kibamba', 'Kimara', 'Makongo Juu', 'Manzese', 'Mwenge', 'Saranga', 'Ubungo'
             ],
-            'Kigamboni' => [
+            'Kigamboni Municipal' => [
                 'Kigamboni', 'Kibada', 'Kisarawe II', 'Mjimwema', 'Somangila', 'Tungi'
             ],
 
@@ -255,15 +261,15 @@ class TanzaniaSeeder extends Seeder
             ],
 
             // Mwanza wards
-            'Ilemela' => [
+            'Ilemela Municipal' => [
                 'Buhongwa', 'Ibungilo', 'Ilemela', 'Kitangiri', 'Ngudu', 'Pasiansi'
             ],
-            'Nyamagana' => [
+            'Mwanza' => [
                 'Bugando', 'Buzuruga', 'Igogo', 'Mahina', 'Mwanza', 'Nyakato', 'Nyamanoro', 'Pamba'
             ],
 
             // Dodoma wards
-            'Dodoma Urban' => [
+            'Dodoma City' => [
                 'Chang\'ombe', 'Chihanga', 'Iyumbu', 'Kiwanja cha Ndege', 'Makole', 'Mkonze', 'Msalato', 'Nala', 'Uhuru', 'Zuzu'
             ],
 
@@ -273,22 +279,22 @@ class TanzaniaSeeder extends Seeder
             ],
 
             // Moshi wards
-            'Moshi Urban' => [
+            'Moshi Municipal' => [
                 'Bondeni', 'Kiboriloni', 'Kilimanjaro', 'Kiusa', 'Korongoni', 'Longuo', 'Majengo', 'Mabogini', 'Msaranga', 'Rau'
             ],
 
             // Iringa wards
-            'Iringa Urban' => [
+            'Iringa Municipal' => [
                 'Gangilonga', 'Kihesa', 'Kitanzini', 'Mivinjeni', 'Mkwawa', 'Ruaha'
             ],
 
             // Bukoba wards
-            'Bukoba Urban' => [
+            'Bukoba Municipal' => [
                 'Hamugembe', 'Ijuganyondo', 'Kahororo', 'Karabagaine', 'Nyakaiga', 'Nyakato'
             ],
 
             // Kigoma wards
-            'Kigoma Urban' => [
+            'Kigoma Municipal' => [
                 'Bangwe', 'Gungu', 'Kalalangabo', 'Katubuka', 'Kigoma', 'Mahembe', 'Mwanga Kaskazini', 'Mwanga Kusini'
             ],
 
@@ -298,33 +304,55 @@ class TanzaniaSeeder extends Seeder
             ],
 
             // Morogoro wards
-            'Morogoro Urban' => [
+            'Morogoro Municipal' => [
                 'Boma', 'Kihonda', 'Kingolwira', 'Mazimbu', 'Mfukulembe', 'Mji Mkuu', 'Mwembesongo', 'Sabasaba'
             ],
 
             // Mtwara wards
-            'Mtwara Urban' => [
+            'Mtwara Municipal' => [
                 'Chumbageni', 'Kombeni', 'Majengo', 'Mchinga', 'Msimbazi', 'Mtanda', 'Shangani', 'Shimo la Udongo'
             ],
 
             // Shinyanga wards
-            'Shinyanga Urban' => [
+            'Shinyanga Municipal' => [
                 'Ibadakuli', 'Kambarage', 'Kitangiri', 'Kolandoto', 'Mabuki', 'Majengo', 'Ndoleleji', 'Shinyanga'
             ],
 
             // Singida wards
-            'Singida Urban' => [
+            'Singida Municipal' => [
                 'Mtipa', 'Mwasauya', 'Ndevelya', 'Singida', 'Uhambo'
             ],
 
             // Tabora wards
-            'Tabora Urban' => [
+            'Tabora Municipal' => [
                 'Cheyo', 'Gongoni', 'Ipala', 'Isevya', 'Kanyenye', 'Kiloleni', 'Ng\'ambo', 'Tumbi'
             ],
 
             // Zanzibar wards
-            'Mjini' => [
+            'Mjini Municipal' => [
                 'Funguni', 'Forodhani', 'Hurumzi', 'Jang\'ombe', 'Karakana', 'Kikwajuni', 'Kiponda', 'Kwahani', 'Malindi', 'Mchangani', 'Meya', 'Mikunguni', 'Mlandege', 'Muembe Makumbi', 'Muembe Wambaa', 'Rahaleo', 'Shangani', 'Shaurimoyo', 'Stone Town', 'Tomondo', 'Vikokotoni'
+            ],
+
+            // Rungwe wards (as provided by user)
+            'Rungwe' => [
+                'Bagamoyo', 'Bujela', 'Bulyaga', 'Ibighi', 'Ikama', 'Ikuti',
+                'Ilima', 'Iponjela', 'Isongole', 'Itagata', 'Kawetele', 'Kikole',
+                'Kinyala', 'Kisiba', 'Kisondela', 'Kiwira', 'Kyimo', 'Lufingo',
+                'Lupepo', 'Makandana', 'Malindo', 'Masebe', 'Masoko', 'Masukulu',
+                'Matwebe', 'Mpuguso', 'Msasani', 'Ndato', 'Nkunga', 'Suma', 'Swaya'
+            ],
+
+            // Additional major towns
+            'Musoma Municipal' => [
+                'Bweri', 'Iringo', 'Kihumbu', 'Makoko', 'Mwisenge', 'Nyamatare', 'Nyasho', 'Pasiansi'
+            ],
+
+            'Sumbawanga Municipal' => [
+                'Katandala', 'Maendeleo', 'Mapanda', 'Mwanga', 'Ntendo', 'Wampembe'
+            ],
+
+            'Songea Municipal' => [
+                'Kigonsera', 'Kichangani', 'Mahaba', 'Matogoro', 'Mzinga', 'Raha'
             ]
         ];
 
@@ -352,7 +380,7 @@ class TanzaniaSeeder extends Seeder
                         'region_id' => $district->region_id,
                         'district_id' => $district->id,
                         'village_id' => $village->id,
-                        'other_villages_details' => "Ward of {$wardName} in {$districtName} district"
+                        'other_ward_details' => "Ward of {$wardName} in {$districtName} district"
                     ]);
                 }
             }
